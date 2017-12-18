f2299173be91e0c06c89662df751d99a20ee60a3
Borgne <ronan.borgne@laposte.net>
2017-12-18 17:19:52 +0100

    sese

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -249,7 +249,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :facebook, '', '', :scope => 'email'
+  config.omniauth :facebook, '192874564596288', '048951fbd0e9d44e844580ecfd68b0da', :scope => 'email'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
